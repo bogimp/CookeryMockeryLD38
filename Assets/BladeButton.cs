2ffb6db9f0c6d770a138b762a404ebc06afcd9ef
yar <bog.imp@gmail.com>
2017-04-23 02:38:32 +0300

    + dottween + blade cutter

---
+++ Assets/BladeButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class BladeButton : MonoBehaviour
+{
+    public GameObject Blade;
+    public BladeCutter BladeCutter;
+
+    public float BladeMoveDy = -0.8f;
+
+    private bool _ready;
+    private bool _cutted;
+
+    public void Start()
+    {
+        _ready = true;
+        _cutted = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!_ready) return;
+
+        _cutted = false;
+        _ready = false;
+
+        var posA = Blade.transform.position + new Vector3(0, BladeMoveDy, 0f);
+        var posB = Blade.transform.position;
+
+        var sequence = DOTween.Sequence();
+        sequence.Append(Blade.transform.DOMove(posA, 1f));
+        sequence.Append(Blade.transform.DOMove(posB, 1f));
+        sequence.InsertCallback(1f, OnBlade);
+        sequence.OnComplete(OnComplete);
+    }
+
+    private void OnComplete()
+    {
+        _ready = true;
+    }
+
+    public void OnBlade()
+    {
+        if (_cutted) return;
+
+        _cutted = true;
+        BladeCutter.Cut();
+    }
+}
---
+++ Assets/BladeCutter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class BladeCutter : MonoBehaviour
+{
+    public Material capMaterial;
+    public void Cut()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+
+            GameObject victim = hit.collider.gameObject;
+
+            GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+            foreach (var piece in pieces)
+            {
+                if (!piece.GetComponent<Rigidbody>())
+                {
+                    piece.AddComponent<Rigidbody>();
+                }
+                var coll = piece.GetComponent<MeshCollider>();
+                if (!coll)
+                {
+                    coll = piece.AddComponent<MeshCollider>();
+                }
+                var meshFilter = piece.GetComponent<MeshFilter>();
+                coll.sharedMesh = meshFilter.mesh;
+                coll.inflateMesh = true;
+                coll.skinWidth = 0.01f;
+                coll.convex = true;
+            }
+
+        }
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5.0f);
+        Gizmos.DrawLine(transform.position + transform.up * 0.5f, transform.position + transform.up * 0.5f + transform.forward * 5.0f);
+        Gizmos.DrawLine(transform.position + -transform.up * 0.5f, transform.position + -transform.up * 0.5f + transform.forward * 5.0f);
+
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * 0.5f);
+        Gizmos.DrawLine(transform.position, transform.position + -transform.up * 0.5f);
+
+    }
+}
