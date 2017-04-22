ce3b9161e56f4d5e8b5bec95eaa041e5576e04c9
yar <bog.imp@gmail.com>
2017-04-22 20:51:25 +0300

    + effect + input

---
+++ Assets/Controlls.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.InputNew;
+
+// GENERATED FILE - DO NOT EDIT MANUALLY
+public class Controlls : ActionMapInput {
+	public Controlls (ActionMap actionMap) : base (actionMap) { }
+	
+	public ButtonInputControl @moveX { get { return (ButtonInputControl)this[0]; } }
+}
